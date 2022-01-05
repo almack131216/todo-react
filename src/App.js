790f74168eb59fb7cee055e3aaed7d2ebb071415
ecp-alex <alex.mackenzie@englishcollege.cz>
2022-01-05 20:45:57 +0100

    UI | add html + css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react"
 import TodoList from "./TodoList"
 import { v4 as uuidv4 } from "uuid"
+import "./App.css"
 
 const LOCAL_STORAGE_KEY = "todoApp.todos"
 
@@ -11,13 +12,16 @@ function App() {
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     if (storedTodos) setTodos(storedTodos)
+    console.log("[useEffect][1]", todos)
   }, [])
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    console.log("[useEffect][2]", todos)
   }, [todos])
 
   function toggleTodo(id) {
+    console.log("[toggleToDo]")
     const newTodos = [...todos]
     const todo = newTodos.find((todo) => todo.id === id)
     todo.complete = !todo.complete
@@ -40,11 +44,76 @@ function App() {
 
   return (
     <>
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input type='text' ref={todoNameRef} />
-      <button onClick={handleAddTodo}>Add</button>
-      <button onClick={handleClearTodos}>Clear completed</button>
-      <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
+      <h1 className='title'>Stuff I need to do</h1>
+      <div className='all-tasks'>
+        <h2 className='task-list-title'>My lists</h2>
+        {/* dynamic population */}
+        <ul className='task-list' data-lists></ul>
+        {/* /dynamic population */}
+
+        <form action='' data-new-list-form>
+          <input
+            type='text'
+            className='inp-new list'
+            data-new-list-input
+            placeholder='new list name'
+            aria-label='new list name'
+          />
+          <button className='btn create' aria-label='create new list'>
+            +
+          </button>
+        </form>
+      </div>
+      {/* /all-tasks */}
+
+      <div className='todo-list' data-list-display-container>
+        <div className='todo-header'>
+          <h2 className='list-title' data-list-title>
+            My Tasks
+          </h2>
+          <p className='task-count' data-list-count>
+            {todos.filter((todo) => !todo.complete).length} left to do
+          </p>
+        </div>
+        <div className='todo-body'>
+          {/* dynamic population */}
+          <div className='tasks' data-tasks>
+            <TodoList todos={todos} toggleTodo={toggleTodo} />
+          </div>
+          {/* /dynamic population */}
+          <div className='new-task-creator'>
+            <form action='' data-new-task-form>
+              <input
+                type='text'
+                data-new-task-input
+                className='inp-new task'
+                placeholder='new task name'
+                aria-label='new task name'
+                ref={todoNameRef}
+              />
+              <button
+                className='btn create'
+                aria-label='create new task'
+                onClick={handleAddTodo}
+              >
+                +
+              </button>
+            </form>
+          </div>
+          <div className='delete-stuff'>
+            <button
+              className='btn delete'
+              data-clear-complete-tasks-button
+              onClick={handleClearTodos}
+            >
+              Clear completed tasks
+            </button>
+            <button className='btn delete' data-delete-list-button>
+              Delete list
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/Todo.js
+++ src/Todo.js
@@ -2,17 +2,20 @@ import React from "react"
 
 export default function Todo({ todo, toggleTodo }) {
   function handleTodoClick() {
+    console.log("[handleTodoClick]")
     toggleTodo(todo.id)
   }
 
   return (
-    <div>
-      <label htmlFor=''>
-        <input
-          type='checkbox'
-          checked={todo.complete}
-          onChange={handleTodoClick}
-        />
+    <div className='task'>
+      <input
+        type='checkbox'
+        id={todo.id}
+        checked={todo.complete}
+        onChange={handleTodoClick}
+      />
+      <label htmlFor={todo.id}>
+        <span className='custom-checkbox'></span>
         {todo.name}
       </label>
     </div>
